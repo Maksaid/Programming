7fd01f8f6c7e3800896a34a45a73fd0b74bfd098
maksaid <mapinchuk12@gmailcom>
2021-11-14 18:23:34 +0300

    8lab

---
+++ 8lab/cmake-build-debug/CMakeFiles/8lab.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "8lab.exe"
+  "8lab.exe.manifest"
+  "8lab.pdb"
+  "CMakeFiles/8lab.dir/main.c.obj"
+  "lib8lab.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/8lab.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
